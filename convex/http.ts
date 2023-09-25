04e5561db90ad6fbba9be74d6ff5dfa91d7a4ace
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-09-25 12:41:48 +0100

    added typescript definitions for node,created http file in convex

---
+++ convex/http.ts
@@ -0,0 +1,30 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { api } from "./_generated/api";
+
+const http = httpRouter();
+
+http.route({
+  path: "/sendImage",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => {
+    // Step 1: Store the file
+    const blob = await request.blob();
+    const storageId = await ctx.storage.store(blob);
+
+    // Step 2: Save the storage ID to the database via a mutation
+    const author = new URL(request.url).searchParams.get("author");
+    await ctx.runMutation(api.messages.sendImage, { storageId, author });
+
+    // Step 3: Return a response with the correct CORS headers
+    return new Response(null, {
+      status: 200,
+      // CORS headers
+      headers: new Headers({
+        // e.g. https://mywebsite.com
+        "Access-Control-Allow-Origin": process.env.CLIENT_ORIGIN!,
+        Vary: "origin",
+      }),
+    });
+  }),
+});
